c5184be18085ba16eb997425471b7c318dc719ca
Philippe Boneff <phb@google.com>
2025-03-11 13:59:58 +0000

    edit url

--- deployment/modules/gcp/cloudbuild/preloaded/main.tf
+++ deployment/modules/gcp/cloudbuild/preloaded/main.tf
@@ -109,6 +109,9 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       script = <<EOT
         terragrunt --terragrunt-non-interactive --terragrunt-no-color apply -auto-approve -no-color 2>&1
         terragrunt --terragrunt-no-color output --raw conformance_url -no-color > /workspace/conformance_url
+	cat /workspace/conformance_url
+	sed -i 's-https://--g' /workspace/conformance_url
+	cat /workspace/conformance_url
         terragrunt --terragrunt-no-color output --raw conformance_bucket_name -no-color > /workspace/conformance_bucket_name
         terragrunt --terragrunt-no-color output --raw ecdsa_p256_public_key_data -no-color > /workspace/conformance_log_public_key.pem
       EOT
