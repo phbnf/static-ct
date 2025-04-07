c40f0a19582a9e65b22c263920ed79ea31142a81
Philippe Boneff <phb@google.com>
2025-04-07 14:23:33 +0000

    run more tesseract servers

--- deployment/modules/gcp/cloudrun/main.tf
+++ deployment/modules/gcp/cloudrun/main.tf
@@ -31,7 +31,8 @@ resource "google_cloud_run_v2_service" "default" {
     timeout                          = "5s"
 
     scaling {
-      max_instance_count = 3
+      max_instance_count = 6
+      min_instance_count = 3
     }
 
     containers {
